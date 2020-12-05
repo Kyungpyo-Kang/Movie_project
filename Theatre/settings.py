65454bcf13e68e7c8d2371d21d04d872b06bca37
Kyungpyo Kang <kyungpyokang@Kyungpyoui-iMac.local>
2020-12-05 23:02:04 +0900

    allowed host update

--- Theatre/settings.py
+++ Theatre/settings.py
@@ -8,7 +8,9 @@ SECRET_KEY = 'f70%n6wvd%z*s6r&mh1jubv@7yhrsl-jb*fvp=92l_py=)6o%6'
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".ap-northeast-2.compute.amazonaws.com"
+]
 
 
 # Application definition
